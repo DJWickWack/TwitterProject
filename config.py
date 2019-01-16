3d877b8d70d74ea8b9827948e47d934e788747a0
DJWickWack <35749854+DJWickWack@users.noreply.github.com>
2019-01-16 09:54:49 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+class keys():
+    consumer_key="WTlR4vdaXPAx15eRKzarFFK5M"
+    consumer_secret="V3stPYHjdQVCKEZE8dqzZNOzNJndaoX2rcI4pRAf090dBmLrf6"
+    
+    access_token="1041353482794500097-T0WUVf07qWlHw13VG8t5gvpw24KwC0"
+    access_token_secret="OHPDCoO6qHWpysPSMuO1L3HUi4ewY1l034VCeGWyqXR3Q"
+    b=1
+    
